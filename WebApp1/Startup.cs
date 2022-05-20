3bccc091f3ff95789d8cf22924bc42f096410ce6
Lâm Khải <34184399+lamkhai@users.noreply.github.com>
2022-05-20 16:09:26 +0700

    Remove AddDbContext and AddDefaultIdentity in Startup.ConfigureServices

--- WebApp1/Startup.cs
+++ WebApp1/Startup.cs
@@ -27,12 +27,7 @@ namespace WebApp1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
 
             services.Configure<IdentityOptions>(options =>
